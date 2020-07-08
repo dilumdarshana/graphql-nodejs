4495ba85eecfb2817a80919f241394ab278dae71
Dilum Darshana <dilum.dar@gmail.com>
2020-07-08 15:00:18 +0530

    Docker setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# NodeJs connect with Graphql
+
+How run application
+- docker-compose run --service-ports --rm graphql_node yarn start
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
-app.listen(8000, () => {
-    console.log('Listening...');
-});
+mongoose
+    .connect('mongodb://graphql_mongo:27017/lyrics', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
+        app.listen(8000, () => {
+            console.log('**** Connected to MongoDB ****');
+            console.log('**** Listening on port 8000 ****');
+        });
+    })
+    .catch(error => {
+        console.log('Error on connecting to MongoDB', error);
+    });
